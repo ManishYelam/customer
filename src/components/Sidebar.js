9b82140f6db49ee363e119ba9814f4e563028e9a
ManishYelam <manishyelam@gmail.com>
2024-08-12 16:57:31 +0530

    yes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';d
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Sidebar.css'; 
 
 function Sidebar() {
